41c4d177bf49ed5c003c0cd7fedae15b81572047
Tetiana <tetianakovpak@gmail.com>
2025-12-14 18:34:20 +0200

    Remove comment

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import "modern-normalize/modern-normalize.css";
 import App from "./components/App/App.tsx";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-// import ReactPaginate from "react-paginate";
 
 const queryClient = new QueryClient();
 
